f482aaccd0a62e32c756b6e47c1df4339ac2be38
YSThomas <tombrazy@gmail.com>
2021-03-12 09:21:09 +0700

    nothing... really

--- js/main.js
+++ js/main.js
@@ -4,37 +4,63 @@ const modalBtn = document.querySelector(".m-btn");
 const modalBg = document.querySelector(".m-bg");
 const modalClose = document.querySelector(".m-bgclose");
 
-const modalDiv = document.getElementsByClassName("calendar__dates-date");
+const modalDiv = document.querySelectorAll(".calendar__dates-date");
 
-let todoInput = document.querySelector(".toDo-input");
+const todoInput = document.querySelector(".toDo-input");
 const todoBtn = document.querySelector(".toDo-add");
 const todoList = document.querySelector(".toDo-list");
 
-for (let i = 0; i < modalDiv.length; i++) {
-  let item = modalDiv[i];
-
-  let listTodo = [];
-
-  item.addEventListener("click", function () {
-    console.log(item);
-
+const modalDivArray = Array.from(modalDiv);
+/*
+modalDivArray.forEach((link, index) => {
+  let listTodo = []; //грешу на эту штуку
+  link.addEventListener("click", function () {
     modalBg.classList.add("m-bg__active");
+  });
 
+  todoBtn.addEventListener("click", function () {
+    let input = todoInput.value;
+    let temp = { todo: input, completed: false };
+    //temp.todo = input;
+    //temp.completed = false;
+
+    let i = listTodo.length; //грешу на эту штуку 2
+    listTodo[i] = temp; // и на эту штуку
+  });
+
+  modalClose.addEventListener("click", function () {
+    modalBg.classList.remove("m-bg__active");
+  });
+  console.log(listTodo);
+}); //? возможно ли выйти из такого положения...
+
+
+*/
+
+for (let i = 0; i < modalDivArray.length; i++) {
+  let listTodo = [];
+  modalDivArray[i].addEventListener("click", function () {
+    modalBg.classList.add("m-bg__active");
     todoBtn.addEventListener("click", function () {
       let input = todoInput.value;
-      let temp = {};
-      temp.todo = input;
-      temp.completed = false;
+      let temp = { todo: input, completed: false };
+      //temp.todo = input;
+      //temp.completed = false;
 
       let i = listTodo.length;
-      //listTodo[i] = temp;
-      listTodo.splice(i, 0, temp);
-    });
-    console.log(listTodo);
-  });
-}
+      listTodo[i] = temp;
 
+      modalClose.addEventListener("click", function () {
+        modalBg.classList.remove("m-bg__active");
+        listTodo.length = 0; //? Выход? :D
+      });
+    });
+  });
+  console.log(listTodo);
+}
 /*
+
+
 for(let i=0;i<modalDiv.length;i++){
     modalDiv[i].addEventListener('click', function(e) {
     	console.log(modalDiv);
@@ -42,16 +68,26 @@ for(let i=0;i<modalDiv.length;i++){
 }
 
 
-Array.from(modalDiv).forEach((link) => {
+modalDiv.forEach((link) => {
+  let listTodo = [];
   link.addEventListener("click", function (event) {
     modalBg.classList.add("m-bg__active");
+
+    todoBtn.addEventListener("click", function () {
+      let input = todoInput.value;
+      let temp = {};
+      temp.todo = input;
+      temp.completed = false;
+      let b = listTodo.length;
+      listTodo[b] = temp;
+      console.log(listTodo);
+      return;
+      //listTodo.splice(i, 0, temp);
+    });
   });
   //lol
 });
 */
-modalClose.addEventListener("click", function () {
-  modalBg.classList.remove("m-bg__active");
-});
 
 todoBtn.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTask);
