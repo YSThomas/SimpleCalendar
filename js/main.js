bd7f3c7aea9eec75561d81a2dc6d279951131cab
YSThomas <tombrazy@gmail.com>
2021-03-29 17:05:37 +0700

    render

--- js/main.js
+++ js/main.js
@@ -24,47 +24,70 @@ for (let i = 0; i < modalDiv.length; i++) {
     console.log(window.selectedDay);
 
     modalBg.classList.add("m-bg__active");
+
+    let arrayItems = allDatesArray[window.selectedDay - 1].reduce(
+      (result, item) => {
+        result += `
+        <div class="todo"><li class="todo-item">
+      ${item["todo"]}
+      </li>
+      <button class="complete-btn">Сделано</button><button class="delete-btn">Удалить</button></div>
+        `;
+        console.log(item);
+        return result;
+      },
+      ""
+    );
+
+    todoList.innerHTML = arrayItems;
+
+    // for (let j = 0; j < window.selectedDay; j++) {
+    //   let out = "";
+    //   out += `
+    // <div class="todo"><li class="todo-item">
+    // ${allDatesArray[window.selectedDay - 1][0]["todo"]}
+    // </li>
+    // <button class="complete-btn">Сделано</button><button class="delete-btn">Удалить</button></div>`;
+    //   todoList.innerHTML = out;
+    // }
   });
 }
 
 console.log(allDatesArray);
 
-todoBtn.addEventListener("click", function () {
+todoBtn.addEventListener("click", function (e) {
   let input = todoInput.value;
   let temp = { todo: input, completed: false };
+  let objIndex = allDatesArray[window.selectedDay - 1].length;
 
   allDatesArray[window.selectedDay - 1].push(temp);
+  addTodo(e, objIndex);
 });
 
 modalClose.addEventListener("click", function () {
   modalBg.classList.remove("m-bg__active");
-
-  window.selectedDay = 0;
 });
 
-todoBtn.addEventListener("click", addTodo);
+// todoBtn.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTask);
 
-function addTodo(event) {
+function addTodo(event, index) {
+  // allDatesArray[window.selectedDay - 1];
+
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
-
   const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value;
+  newTodo.innerText = allDatesArray[window.selectedDay - 1][index]["todo"];
   newTodo.classList.add("todo-item");
-
   todoDiv.appendChild(newTodo);
-
   const completedBtn = document.createElement("button");
   completedBtn.innerText = "Сделано";
   completedBtn.classList.add("complete-btn");
   todoDiv.appendChild(completedBtn);
-
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Удалить";
   deleteBtn.classList.add("delete-btn");
   todoDiv.appendChild(deleteBtn);
-
   todoList.appendChild(todoDiv);
 }
 
