6048c0d0392fd0dcc0192083fcabff9415afdd65
YSThomas <tombrazy@gmail.com>
2021-03-31 08:26:15 +0700

    delete btn

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ for (let i = 0; i < modalDiv.length; i++) {
 
   allDatesArray.push(listTodo);
 
-  item.addEventListener("click", function () {
+  item.addEventListener("click", function (e) {
     window.selectedDay = Number(item.innerText);
     console.log(window.selectedDay);
 
@@ -31,9 +31,11 @@ for (let i = 0; i < modalDiv.length; i++) {
         <div class="todo"><li class="todo-item">
       ${item["todo"]}
       </li>
-      <button class="complete-btn">Сделано</button><button class="delete-btn">Удалить</button></div>
+      <div>
+        <button class="complete-btn">Сделано</button>
+        <button class="delete-btn">Удалить</button></div>
+      </div>
         `;
-        console.log(item);
         return result;
       },
       ""
@@ -61,8 +63,29 @@ todoBtn.addEventListener("click", function (e) {
   let objIndex = allDatesArray[window.selectedDay - 1].length;
 
   allDatesArray[window.selectedDay - 1].push(temp);
+
   addTodo(e, objIndex);
+
+  // objIndex = objIndex;
+  // chtoTo(objIndex);
 });
+
+function chtoTo(index) {
+  const deleteBtn = document.getElementsByClassName("delete-btn");
+  const todoElement = document.querySelector(".todo");
+  // for (let i = 0; i < deleteBtn.length; i++) {
+  console.log(index);
+  deleteBtn[index].addEventListener("click", (e) => {
+    // delete allDatesArray[window.selectedDay - 1][i];
+    let array = allDatesArray[window.selectedDay - 1];
+    array.splice(index, 1);
+
+    console.log(array);
+
+    console.log(index);
+  });
+  // }
+}
 
 modalClose.addEventListener("click", function () {
   modalBg.classList.remove("m-bg__active");
@@ -80,27 +103,88 @@ function addTodo(event, index) {
   newTodo.innerText = allDatesArray[window.selectedDay - 1][index]["todo"];
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
+  const btnDiv = document.createElement("div");
+  todoDiv.appendChild(btnDiv);
   const completedBtn = document.createElement("button");
   completedBtn.innerText = "Сделано";
   completedBtn.classList.add("complete-btn");
-  todoDiv.appendChild(completedBtn);
+  btnDiv.appendChild(completedBtn);
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Удалить";
   deleteBtn.classList.add("delete-btn");
-  todoDiv.appendChild(deleteBtn);
+
+  // deleteBtn.addEventListener("click", (e) => {
+  //   index = index;
+  //   console.log(index);
+  // });
+
+  btnDiv.appendChild(deleteBtn);
+
   todoList.appendChild(todoDiv);
+
+  // chtoTo(index);
 }
 
 function deleteTask(e) {
   const item = e.target;
+  const completedBtn = document.createElement("button");
+  let btnText = item.parentElement.innerText;
+  let fullText = item.parentElement.parentElement.innerText;
+  let replaceText = fullText.replace(btnText, "");
+
+  // let arr = allDatesArray[window.selectedDay - 1];
+
+  // let btnText = item.parentElement.innerText;
+  // let fullText = item.parentElement.parentElement.innerText;
+  // let replaceText = fullText.replace(btnText, "");
+  // console.log(replaceText);
+
+  // const ohBlya = arr.filter((el) => {
+  //   return el["todo"].indexOf(replaceText);
+  // });
+  // console.log(ohBlya);
+
+  // let getIndex = arr["todo"].indexOf(replaceText);
+
+  // chtoTo();
+  allDatesArray[window.selectedDay - 1].forEach((obj, i) => {
+    console.log(obj);
+    if (
+      replaceText.replace(/\r?\n/g, "") ==
+      allDatesArray[window.selectedDay - 1][i].todo
+    ) {
+      allDatesArray[window.selectedDay - 1].splice(i, 1);
+      console.log("Получилось! Индекс: " + i);
+    } else {
+      console.log("Снова что-то поломал");
+      return;
+    }
+    // console.log(obj["todo"]);
+  });
 
   if (item.classList[0] === "delete-btn") {
-    const todo = item.parentElement;
+    const todo = item.parentElement.parentElement;
+
     todo.remove();
   }
 
   if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
+    const todo = item.parentElement.parentElement;
     todo.classList.toggle("completed");
   }
 }
+
+// const deleteBtn = document.getElementsByClassName("delete-btn");
+
+// document.addEventListener("click", function (e) {
+//   if (e.target && e.target.className == "delete-btn") {
+//     console.log(deleteBtn);
+
+//   }
+// });
+
+// for (let i = 0; i < deleteBtn.length; i++) {
+// deleteBtn[i].addEventListener("click", (e) => {
+//   console.log(e);
+// });
+// }
